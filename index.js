96a7f9235ff0ac3a0c577c84efc806727cd0c5ba
joshuaWithey <83647960+joshuaWithey@users.noreply.github.com>
2022-08-18 14:40:40 +1000

    Functions

---
+++ index.js
@@ -0,0 +1,4 @@
+const now = new Date();
+const yesterday = new Date();
+//yesterday.setDate(now.getDate - 1);
+console.log(now, yesterday);
--- src/api.js
+++ src/api.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const { APIClient } = require("ig-trading-api");
+require("dotenv").config();
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
+router.get("/", async (req, res) => {
+  const client = new APIClient(APIClient.URL_DEMO, process.env.IG_APIKEY);
+  const session = await client.rest.login.createSession(
+    process.env.IG_USERNAME,
+    process.env.IG_PASSWORD
+  );
+  console.time("Start");
+  // Get account
+  const accounts = await client.rest.account.getAccounts();
+
+  // Get positions
+  const positions = await client.rest.dealing.getAllOpenPositions();
+  console.timeEnd("Start");
+
+  // Get previous trading history
+  const now = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(now.getDate() - 1);
+  const history = await client.rest.account.getActivityHistory({
+    detailed: true,
+    from: yesterday.toISOString(),
+    to: now.toISOString(),
+  });
+
+  res.status(200).json({
+    account: accounts,
+    positions: positions,
+    //history: history,
   });
 });
-
-router.get("/test", (req, res) => {
-    res.json({
-      hello: "test"
-    });
-  });
 
 app.use(`/.netlify/functions/api`, router);
 
